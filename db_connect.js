74e9c98384431eeffe1b9d0405c242939b89955e
Quievreux <jquievreux.devpro@gmail.com>
2021-11-30 19:54:24 +0100

    Update db_connect.js

--- db_connect.js
+++ db_connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URI = process.env.MONGO_URL || "mongodb+srv://JQuievreux:Erin25012017@cluster0.mxqak.mongodb.net/Portfolio" 
+const DB_URI = process.env.MONGO_URL
 
 mongoose.connect(DB_URI).then(()=>{
 console.log("connected to : "+DB_URI);
