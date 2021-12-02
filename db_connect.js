7db16087dd40f40192e91b34c60c2b48696d02bf
Quievreux <jquievreux.devpro@gmail.com>
2021-12-03 00:29:47 +0100

    new ENV db

--- db_connect.js
+++ db_connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URI = process.env.MONGO_URL
+const DB_URI = process.env.MONGODB_PORTFOLIO_JQ
 
 mongoose.connect(DB_URI).then(()=>{
 console.log("connected to : "+DB_URI);
